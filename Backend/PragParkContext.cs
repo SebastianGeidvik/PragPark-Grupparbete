b41d3712f227410aeed24b4ded735ce6be355c7c
bjorne2000 <bjornis123@live.se>
2021-05-11 11:31:45 +0200

    removebutton

--- Backend/PragParkContext.cs
+++ Backend/PragParkContext.cs
@@ -14,7 +14,11 @@ namespace Backend
                 //optionsBuilder.UseLazyLoadingProxies();
 
                 //Tintin
-                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-FPE71HP\SQLEXPRESS;Database=PragParkWinform;Trusted_Connection=True;MultipleActiveResultSets=true");
+                //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-FPE71HP\SQLEXPRESS;Database=PragParkWinform;Trusted_Connection=True;MultipleActiveResultSets=true");
+                //optionsBuilder.UseLazyLoadingProxies();
+
+                //Björn
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Database=PragParkWinform;Trusted_Connection=True;MultipleActiveResultSets=true");
                 optionsBuilder.UseLazyLoadingProxies();
             }
         }
--- WinFormsApp3/RemoveVehicle.Designer.cs
+++ WinFormsApp3/RemoveVehicle.Designer.cs
@@ -37,39 +37,40 @@ namespace WinFormsApp3
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(115, 185);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(131, 247);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 20);
+            this.button1.Size = new System.Drawing.Size(125, 27);
             this.button1.TabIndex = 0;
             this.button1.Text = "Remove";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(115, 123);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(131, 164);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 23);
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(115, 106);
+            this.label2.Location = new System.Drawing.Point(131, 141);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.Size = new System.Drawing.Size(132, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Enter License Plate";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(8, 307);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(9, 409);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(78, 20);
+            this.button2.Size = new System.Drawing.Size(89, 27);
             this.button2.TabIndex = 4;
             this.button2.Text = "Return";
             this.button2.UseVisualStyleBackColor = true;
@@ -77,16 +78,16 @@ namespace WinFormsApp3
             // 
             // RemoveVehicle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(335, 334);
+            this.ClientSize = new System.Drawing.Size(383, 445);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.MaximizeBox = false;
             this.Name = "RemoveVehicle";
             this.ShowIcon = false;
